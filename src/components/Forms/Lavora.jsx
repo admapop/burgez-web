763e460e0b0a8b1fcda0d81da0df2a5a6a037dc2
admapop <52109936+admapop@users.noreply.github.com>
2019-09-09 21:27:16 +0200

    Update Lavora.jsx
    Added code to process form

--- src/components/Forms/Lavora.jsx
+++ src/components/Forms/Lavora.jsx
@@ -5,17 +5,38 @@ import Logo from '../Logo/Logo';
 
 import './Lavora.scss'
 
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
+
 export default class Lavora extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            value: ''
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
         }
     }
 
     handleChange = (event) => {
-        this.setState({ value: event.target.value });
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "lavora", ...this.state })
+        })
+            .then(() => alert("Success!"))
+            .then(() => this.setState({ name: '', email: '', subject: '', message: '' }))
+            .catch(error => alert(error));
+        event.preventDefault();
     }
 
     render() {
@@ -52,22 +73,22 @@ export default class Lavora extends Component {
                         <div className="fl w-40" style={{ overflow: "hidden" }} id="contactform">
                             <div className="wrapper">
                                 <h1 className="f1 i" id="desktop">CONTACT</h1>
-                                <form className="" onSubmit={this.handleSubmit}>
+                                <form name="lavora" onSubmit={this.handleSubmit}>
                                     <div className="desktop">
                                         <label htmlFor="name" className="fl w-90 f2 fw4 b db mb2 ">NOME E COGNOME</label>
-                                        <input className="w-100 bn pv2" type="text" name="name" id="name" value={this.state.value} onChange={this.handleChange} />
+                                        <input className="w-100 bn pv2" type="text" name="name" id="name" value={name} onChange={this.handleChange} />
                                     </div>
                                     <div className="desktop">
                                         <label htmlFor="email" className="fl w-90 f2 fw4 b db mb2 ">EMAIL</label>
-                                        <input className="w-100 bn pv2" type="email" name="email" id="email" value={this.state.value} onChange={this.handleChange} />
+                                        <input className="w-100 bn pv2" type="email" name="email" id="email" value={email} onChange={this.handleChange} />
                                     </div>
                                     <div className="desktop">
                                         <label htmlFor="subject" className="fl w-90 f2 fw4 b db mb2 ">OGGETTO</label>
-                                        <input className="w-100 bn pv2" type="text" name="subject" id="subject" value={this.state.value} onChange={this.handleChange} />
+                                        <input className="w-100 bn pv2" type="text" name="subject" id="subject" value={subject} onChange={this.handleChange} />
                                     </div>
                                     <div className="desktop">
                                         <label htmlFor="message" className="fl w-90 f2 fw4 b db mb2 ">MESSAGGIO</label>
-                                        <textarea className="w-100 bn pv2" type="text" name="message" id="message" value={this.state.value} onChange={this.handleChange} />
+                                        <textarea className="w-100 bn pv2" type="text" name="message" id="message" value={message} onChange={this.handleChange} />
                                     </div>
                                     <div><input className="fr" type="submit" value="INVIA" id="submit" /></div>
                                 </form>
@@ -113,23 +134,21 @@ export default class Lavora extends Component {
                         <form className="" onSubmit={this.handleSubmit}>
                             <div className="tablet">
                                 <label htmlFor="name" className="fl w-90 f2 fw4 b db mb2">NOME E COGNOME</label>
-                                <input className="w-90 bn pv2" type="text" name="name" id="name" value={this.state.value} onChange={this.handleChange} />
+                                <input className="w-90 bn pv2" type="text" name="name" id="name" value={name} onChange={this.handleChange} />
                             </div>
                             <div className="tablet">
                                 <label htmlFor="email" className="fl w-90 f2 fw4 b db mb2">EMAIL</label>
-                                <input className="w-100 bn pv2" type="email" name="email" id="email" value={this.state.value} onChange={this.handleChange} />
+                                <input className="w-100 bn pv2" type="email" name="email" id="email" value={email} onChange={this.handleChange} />
                             </div>
                             <div className="tablet">
                                 <label htmlFor="subject" className="fl w-90 f2 fw4 b db mb2">OGGETTO</label>
-                                <input className="w-100 bn pv2" type="text" name="subject" id="subject" value={this.state.value} onChange={this.handleChange} />
+                                <input className="w-100 bn pv2" type="text" name="subject" id="subject" value={subject} onChange={this.handleChange} />
                             </div>
                             <div className="tablet">
                                 <label htmlFor="message" className="fl w-90 f2 fw4 b db mb2">MESSAGGIO</label>
-                                <textarea className="w-100 bn pv2" type="text" name="message" id="message" value={this.state.value} onChange={this.handleChange} />
+                                <textarea className="w-100 bn pv2" type="text" name="message" id="message" value={message} onChange={this.handleChange} />
                             </div>
                             <div><input className="fr" type="submit" value="INVIA" id="submit" /></div>
-
-
                         </form>
                     </div>
                 </Breakpoint>
